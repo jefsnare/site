fc1ad379967c9ab5b13a307fa25fffc64f238595
Jeffrey de Graaf <jegr@avivasolutions.nl>
2020-04-21 21:44:30 +0200

    lazyimages plugin

--- .eleventy.js
+++ .eleventy.js
@@ -5,11 +5,13 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const lazyImagesPlugin = require('eleventy-plugin-lazyimages');
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
   eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(lazyImagesPlugin);
 
   eleventyConfig.setDataDeepMerge(true);
 
